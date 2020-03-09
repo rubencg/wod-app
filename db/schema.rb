5f141e5f86188a8ae780a6881ac6577c0f035363
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-03-09 08:21:44 -0600

    Added is_main to Workouts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_152635) do
+ActiveRecord::Schema.define(version: 2020_03_09_141939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_03_08_152635) do
     t.text "Description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_main"
   end
 
 end
