56c52e782aad769dab18d42e498e088355c5d0ab
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-04-16 22:17:25 -0500

    Added category scaffold and 3 main categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create categories
+Category.create!(name: "Rich Froning", description: "All these wods come from the SugarWod paid app from Rich Froning")
+Category.create!(name: "Benchmark", description: "	All these wods are pre-defined crossfit wods")
+Category.create!(name: "Custom", description: "These are wods that we create out of other sources like instagram, twitter, browsing the web, etc.")
+
+puts "Created 3 initial categories"
